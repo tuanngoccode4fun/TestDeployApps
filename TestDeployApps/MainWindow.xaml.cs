eece3449eb27d40c92daebd141d0e864065df8d1
tuanngoc.n <tuanngoccode4fun@gmail.com>
2020-06-13 11:59:19 +0700

    Add project files.

---
+++ TestDeployApps/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestDeployApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btStart_Click(object sender, RoutedEventArgs e)
+        {
+            string input = "nguyen tuan ngoc";
+            ShowWords(input);
+        }
+        private static void ShowWords(string s)
+        {
+            string pattern = @"n";
+            var matches = Regex.Matches(s, pattern);
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("\nNo words were identified in your input.");
+            }
+            else
+            {
+                Console.WriteLine($"\nThere are {matches.Count} words in your string:");
+                for (int ctr = 0; ctr < matches.Count; ctr++)
+                {
+                    Console.WriteLine($"   #{ctr,2}: '{matches[ctr].Value}' at position {matches[ctr].Index}");
+                }
+            }
+        }
+    }
+        
+}
